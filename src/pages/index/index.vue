f4047b9f3293279130fdd8c894cc28c107655869
(wecychen <492090230@qq.com>
2021-10-12 16:48:52 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <custom-component ref="root">This is a root element</custom-component>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const root = ref(null)
+    onMounted(() => {
+      // DOM 元素将在初始渲染后分配给 ref
+      console.log(root.value) // <div>This is a root element</div>
+    })
+
+    return {
+      root,
+    }
+  },
+}
+</script>
